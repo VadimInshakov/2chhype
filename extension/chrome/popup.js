3caee44badcb6a70e8a86c39f504f4c5a8a6708a
VadimInshakov <vadiminshakov@gmail.com>
2018-02-16 18:27:59 +0430

    boards added

--- extension/chrome/popup.js
+++ extension/chrome/popup.js
@@ -18,6 +18,9 @@ window.onload = function() {
         var b = document.getElementById("b");
         var po = document.getElementById("po");
         var pr = document.getElementById("pr");
+        var news = document.getElementById("news");
+        var vg = document.getElementById("vg");
+        var a = document.getElementById("a");
         var root = document.querySelector('#root');
 
         b.addEventListener('click', function () {
@@ -38,17 +41,37 @@ window.onload = function() {
             }
             sendRequest('pr');
         })
+        news.addEventListener('click', function () {
+            while (root.firstChild) {
+                root.removeChild(root.firstChild);
+            }
+            sendRequest('news');
+        })
+        vg.addEventListener('click', function () {
+            while (root.firstChild) {
+                root.removeChild(root.firstChild);
+            }
+            sendRequest('vg');
+        })
+        a.addEventListener('click', function () {
+            while (root.firstChild) {
+                root.removeChild(root.firstChild);
+            }
+            sendRequest('a');
+        })
 
     function sendRequest(board){
        chrome.runtime.sendMessage({board: board}, (response)=>{ 
        response = JSON.parse(response);
        response.forEach((v, i)=>{
+       	if (v.title !== undefined){
          var li = document.createElement('li');
          var a = document.createElement('a');
          a.innerHTML = v.title;
          a.href = v.link;
          a.target="_blank";
          document.querySelector('#root').appendChild(li).appendChild(a); 
+        }
        })
     })
     }
